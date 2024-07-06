cafeeea882f1afb64270d93d7ee603859bc2b1d5
compuser21 <137155283+compuser21@users.noreply.github.com>
2024-07-06 17:45:22 +0400

    Update main.js

--- main.js
+++ main.js
@@ -28,7 +28,6 @@ function fromMorseToNormal() {
     const inputText = document.querySelector('#inputText');
     const outputText = document.querySelector('#outputText');
 
-    // Создаем объект для сопоставления кодов азбуки Морзе с символами алфавита и цифрами
     const morseCodeMap = {
         '.-': 'a', '-...': 'b', '-.-.': 'c', '-..': 'd', '.': 'e', '..-.': 'f', '--.': 'g', '....': 'h', '..': 'i', '.---': 'j',
         '-.-': 'k', '.-..': 'l', '--': 'm', '-.': 'n', '---': 'o', '.--.': 'p', '--.-': 'q', '.-.': 'r', '...': 's', '-': 't',
@@ -36,16 +35,14 @@ function fromMorseToNormal() {
         '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9', ' / ': ' '
     };
 
-    // Преобразуем входную строку Морзе в массив кодов
     const morseArray = inputText.value.trim().split(' ');
 
-    // Преобразуем каждый код Морзе в соответствующий символ
     let text = '';
     for (let morse of morseArray) {
         if (morseCodeMap[morse]) {
             text += morseCodeMap[morse];
         } else {
-            text += ' '; // Если код не найден в объекте, добавляем пробел
+            text += ' '; 
         }
     }
 
